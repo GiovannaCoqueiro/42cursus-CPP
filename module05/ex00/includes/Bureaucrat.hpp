066ff7b2e889cd80b1c7ad77e9fe996082642a2f
Giovanna Coqueiro <gcoqueir@student.42sp.org.br>
2024-04-05 18:36:41 -0300

    module05 ex00

--- module05/ex00/includes/Bureaucrat.hpp
+++ module05/ex00/includes/Bureaucrat.hpp
@@ -6,7 +6,47 @@
 /*   By: gcoqueir <gcoqueir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/25 16:44:56 by gcoqueir          #+#    #+#             */
-/*   Updated: 2024/03/25 16:44:57 by gcoqueir         ###   ########.fr       */
+/*   Updated: 2024/04/05 18:04:10 by gcoqueir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <iostream>
+#include <cstdlib>
+
+class Bureaucrat {
+	private:
+		const std::string _name;
+		int _grade;
+
+    public:
+		class GradeTooLowException : public std::exception {
+			public:
+				virtual const char* what() const throw();
+		};
+
+        class GradeTooHighException : public std::exception {
+	        public:
+		        virtual const char* what() const throw();
+        };
+
+		Bureaucrat();
+		Bureaucrat(const std::string name, int grade)
+			throw(GradeTooHighException, GradeTooLowException);
+		Bureaucrat(const Bureaucrat& rhs);
+		~Bureaucrat();
+		Bureaucrat& operator=(const Bureaucrat& rhs);
+
+		const std::string getName() const;
+		int getGrade() const;
+
+		void increaseGrade();
+		void decreaseGrade();
+		
+};
+
+std::ostream& operator<<(std::ostream& lhs, const Bureaucrat& rhs);
+
+#endif
--- module05/ex00/sources/Bureaucrat.cpp
+++ module05/ex00/sources/Bureaucrat.cpp
@@ -6,7 +6,75 @@
 /*   By: gcoqueir <gcoqueir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/25 16:45:00 by gcoqueir          #+#    #+#             */
-/*   Updated: 2024/03/25 16:45:01 by gcoqueir         ###   ########.fr       */
+/*   Updated: 2024/04/05 18:21:27 by gcoqueir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat() {
+	std::cout << "Default Bureaucrat has been constructed." << std::endl;
+	const_cast<std::string&>(_name) = "Unknown";
+	_grade = (rand() % 150) + 1;
+}
+
+Bureaucrat::Bureaucrat(std::string const name, int grade)
+throw(GradeTooHighException, GradeTooLowException) {
+	std::cout << "Bureaucrat has been constructed." << std::endl;
+    const_cast<std::string&>(_name) = name;
+	if (grade < 1)
+		throw GradeTooHighException();
+	else if (grade > 150)
+		throw GradeTooLowException();
+    _grade = grade;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat& rhs) {
+	std::cout << "Bureaucrat copied." << std::endl;
+	*this = rhs;
+}
+
+Bureaucrat::~Bureaucrat() {
+	std::cout << "Bureaucrat destroyed." << std::endl;
+}
+
+Bureaucrat& Bureaucrat::operator=(const Bureaucrat& rhs) {
+	if(this != &rhs) {
+		const_cast<std::string&>(_name) = rhs._name;
+		_grade = rhs._grade;
+	}
+	return (*this);
+}
+
+const char* Bureaucrat::GradeTooHighException::what() const throw() {
+	return "This grade is too high. The grade must be between 1 and 150.";
+}
+
+const char* Bureaucrat::GradeTooLowException::what() const throw() {
+	return "This grade is too low. The grade must be between 1 and 150.";
+}
+
+const std::string Bureaucrat::getName() const {
+	return _name;
+}
+
+int Bureaucrat::getGrade() const {
+	return _grade;
+}
+
+void Bureaucrat::increaseGrade() {
+	if (_grade - 1 < 1)
+		throw GradeTooHighException();
+	_grade--;
+}
+
+void Bureaucrat::decreaseGrade() {
+	if (_grade + 1 > 150)
+		throw GradeTooLowException();
+	_grade++;
+}
+
+std::ostream& operator<<(std::ostream& lhs, const Bureaucrat& rhs) {
+	lhs << rhs.getName() << ": grade " << rhs.getGrade();
+	return lhs;
+}
