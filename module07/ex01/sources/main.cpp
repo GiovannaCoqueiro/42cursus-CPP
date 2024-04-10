f2d77fd19cf0599f343b695710944894d0ab4f24
Giovanna Coqueiro <giovannacoqueirolopes@gmail.com>
2024-04-10 22:25:50 +0000

    module07 ex00 ex01

---
+++ module07/ex01/sources/main.cpp
@@ -0,0 +1,33 @@
+#include "iter.hpp"
+
+void printInt(int const &i) {
+	std::cout << i << " ";
+}
+
+void printFloat(float const &i) {
+	std::cout << i << " ";
+}
+
+void printString(std::string const &i) {
+	std::cout << i << " ";
+}
+
+int main() {
+	int intArray[] = {1, 2, 3, 4, 5};
+	float floatArray[] = {1.1, 2.2, 3.3, 4.4, 5.5};
+	std::string stringArray[] = {"one", "two", "three", "four", "five"};
+
+	std::cout << "intArray: ";
+	iter<int>(intArray, 5, printInt);
+	std::cout << std::endl;
+
+	std::cout << "floatArray: ";
+	iter<float>(floatArray, 5, printFloat);
+	std::cout << std::endl;
+
+	std::cout << "stringArray: ";
+	iter<std::string>(stringArray, 5, printString);
+	std::cout << std::endl;
+
+	return (0);
+}
