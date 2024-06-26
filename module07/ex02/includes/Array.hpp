e407f73fc565bdaab54b637b662d4d22759a2d66
Giovanna Coqueiro <giovannacoqueirolopes@gmail.com>
2024-06-26 21:07:24 +0000

    07 02

--- module07/ex02/includes/Array.hpp
+++ module07/ex02/includes/Array.hpp
@@ -10,3 +10,25 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+
+#include <iostream>
+
+template< typename T >
+class Array {
+	private:
+		T*		_array;
+		unsigned int	_size;
+
+	public:
+		Array(void);
+		Array(unsigned int n);
+		Array(const Array& rhs);
+		~Array(void);
+		Array& operator=(const Array& rhs);
+		Array& operator[](unsigned int i) throw(std::out_of_range);
+		unsigned int size(void) const;
+};
+
+#endif
--- module07/ex02/sources/Array.cpp
+++ module07/ex02/sources/Array.cpp
@@ -10,3 +10,65 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Array.hpp"
+
+template< typename T >
+Array<T>::Array() {
+	std::cout <<" Default constructor called "<< std::endl;
+	this->_array = NULL;
+	this->_size = 0;
+};
+
+template< typename T >
+Array<T>::Array(unsigned int n) {
+	std::cout << "Parametric constructor called" << std::endl;
+	this->_array = new T[n];
+	for (unsigned int i = 0; i < n; i++) {
+		this->_array[i] = T();
+	}
+	this->_size = n;
+};
+
+template< typename T >
+Array<T>::Array(const Array& rhs){
+	std::cout << "Copy constructor called" << std::endl;
+	this->_array = NULL;
+	*this = rhs;
+};
+
+template< typename T >
+Array<T>::~Array(void){
+	std::cout << "Destructor called" << std::endl;
+	if(this->_array != NULL)
+		delete [] this->_array;
+};
+
+template< typename T >
+Array<T>& Array<T>::operator=(const Array& rhs) {
+	std::cout << "Copy assignment operator called" << std::endl;
+	if(this != &rhs) {
+		if(this->_array != NULL)
+			delete [] this->_array;
+		this->_array = new T[rhs._size];
+		for (unsigned int i = 0; i < rhs._size; i++) {
+			this->_array[i] = rhs._array[i];
+		}
+		this->_size = rhs._size;
+	}
+	return *this;
+};
+
+template< typename T >
+T& Array<T>::operator[](unsigned int i) throw(std::out_of_range) {
+	if (i >= this->_size) {
+		throw std::out_of_range("Index out of range");
+	}
+	if(_array == NULL)
+		throw std::out_of_range("Array is empty");
+	return this->_array[i];
+};
+
+template< typename T >
+unsigned int Array<T>::size(void) const {
+	return this->_size;
+};
