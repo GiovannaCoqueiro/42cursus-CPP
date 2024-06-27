36c99ed008d9c03b604667d6ed5a80d842662b09
Giovanna Coqueiro <gcoqueir@student.42sp.org.br>
2024-06-27 18:56:25 -0300

    module07 ex01 fixed

--- module07/ex01/includes/iter.hpp
+++ module07/ex01/includes/iter.hpp
@@ -6,25 +6,25 @@
 /*   By: gcoqueir <gcoqueir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 15:14:56 by gcoqueir          #+#    #+#             */
-/*   Updated: 2024/04/11 15:14:57 by gcoqueir         ###   ########.fr       */
+/*   Updated: 2024/06/27 18:45:35 by gcoqueir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ITER_HPP
 #define ITER_HPP
 
-template< typename T >
+#include <iostream>
+
+template<typename T>
 void iter(T *array, size_t length, void (*f)(T const &)) {
-	for (size_t i = 0; i < length; i++) {
+	for (size_t i = 0; i < length; i++)
 		f(array[i]);
-	}
 }
 
-template< typename T >
+template<typename T>
 void iter(T const *array, size_t length, void (*f)(T const &)) {
-	for (size_t i = 0; i < length; i++) {
+	for (size_t i = 0; i < length; i++)
 		f(array[i]);
-	}
 }
 
 #endif
--- module07/ex01/sources/main.cpp
+++ module07/ex01/sources/main.cpp
@@ -6,39 +6,39 @@
 /*   By: gcoqueir <gcoqueir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 15:14:59 by gcoqueir          #+#    #+#             */
-/*   Updated: 2024/04/11 15:15:00 by gcoqueir         ###   ########.fr       */
+/*   Updated: 2024/06/27 18:54:08 by gcoqueir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 
 void printInt(int const &i) {
-	std::cout << i << " ";
+	std::cout << i << "\n";
 }
 
 void printFloat(float const &i) {
-	std::cout << i << " ";
+	std::cout << i << "\n";
 }
 
 void printString(std::string const &i) {
-	std::cout << i << " ";
+	std::cout << i << "\n";
 }
 
 int main() {
-	int intArray[] = {1, 2, 3, 4, 5};
+	int intArray[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 	float floatArray[] = {1.1, 2.2, 3.3, 4.4, 5.5};
-	std::string stringArray[] = {"one", "two", "three", "four", "five"};
+	std::string stringArray[] = {"um", "dois", "três"};
 
-	std::cout << "intArray: ";
-	iter<int>(intArray, 5, printInt);
+	std::cout << "intArray:\n";
+	iter<int>(intArray, 10, printInt);
 	std::cout << std::endl;
 
-	std::cout << "floatArray: ";
+	std::cout << "floatArray:\n";
 	iter<float>(floatArray, 5, printFloat);
 	std::cout << std::endl;
 
-	std::cout << "stringArray: ";
-	iter<std::string>(stringArray, 5, printString);
+	std::cout << "stringArray:\n";
+	iter<std::string>(stringArray, 3, printString);
 	std::cout << std::endl;
 
 	return (0);
