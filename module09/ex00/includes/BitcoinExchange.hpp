7b84c03f8cde577cc364782ce7784a12460e161d
Giovanna Coqueiro <gcoqueir@student.42sp.org.br>
2025-01-21 18:25:33 -0300

    .

--- module09/ex00/includes/BitcoinExchange.hpp
+++ module09/ex00/includes/BitcoinExchange.hpp
@@ -6,34 +6,12 @@
 /*   By: gcoqueir <gcoqueir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 15:14:43 by gcoqueir          #+#    #+#             */
-/*   Updated: 2024/04/11 15:14:44 by gcoqueir         ###   ########.fr       */
+/*   Updated: 2025/01/21 18:14:48 by gcoqueir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BITCOINEXCHANGE_HPP
 #define BITCOINEXCHANGE_HPP
-
-// #include <iostream>
-// #include <map>
-// #include <cmath>
-// #include <stdexcept>
-// #include <fstream>
-// #include <sstream>
-
-// class BitcoinExchange {
-// 	private:
-// 		std::map<std::string, double> _exchange;
-// 	public:
-// 		BitcoinExchange();
-// 		BitcoinExchange(const BitcoinExchange& rhs);
-// 		BitcoinExchange& operator=(const BitcoinExchange& rhs);
-// 		~BitcoinExchange();
-
-// 		void readDB(std::string filename);
-//         void createDB(std::string date, double value);
-//         void printDB(std::string date, double rate);
-//         double getValue(std::string date) const;
-// };
 
 # include <iostream>
 # include <fstream>
