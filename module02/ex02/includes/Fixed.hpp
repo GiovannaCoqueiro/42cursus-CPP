a6cd1eb697848e92f6335d080ee4a2d74eb122fc
Giovanna Coqueiro <giovannacoqueirolopes@gmail.com>
2025-06-19 00:06:54 +0000

    .

--- module02/ex02/includes/Fixed.hpp
+++ module02/ex02/includes/Fixed.hpp
@@ -22,19 +22,19 @@ class Fixed {
         static const int _fractBits = 8;
 
     public:
-        Fixed();
-        Fixed(int const num);
-        Fixed(float const num);
-        Fixed(const Fixed& other);
-        ~Fixed();
-        Fixed& operator=(const Fixed& other);
+		Fixed();
+		Fixed(int const num);
+		Fixed(float const num);
+		Fixed(const Fixed& other);
+		~Fixed();
+		Fixed& operator=(const Fixed& other);
 
-        void setRawBits(int const raw);
-        int getRawBits() const;
-        float toFloat() const;
-        int toInt() const;
+		void setRawBits(int const raw);
+		int getRawBits() const;
+		float toFloat() const;
+		int toInt() const;
 
-        static const Fixed min(const Fixed& num1, const Fixed& num2);
+		static const Fixed min(const Fixed& num1, const Fixed& num2);
 		static Fixed min(Fixed& num1, Fixed& num2);
 		static const Fixed max(const Fixed& num1, const Fixed& num2);
 		static Fixed max(Fixed& num1, Fixed& num2);
